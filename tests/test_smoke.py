b6ea3e4450c116b02343a723cc2cf2fd2812c0b9
yunlongxu <yunlong@numagic.io>
2022-03-04 17:56:44 +0100

    change to unittest from pytest

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,13 +1,17 @@
 import numpy as np
 
-def test_add():
-	assert 1+1 == 2
+from unittest import TestCase
 
-def test_casadi_import():
-	import casadi as cas
+class TestSmoke(TestCase):
+	def test_selfadd(self):
+		assert 1+1 == 2
 
-def test_cyipopt_import():
-	import cyipopt
+	def test_casadi_import(self):
+		import casadi as cas
 
-def test_jax_import():
-	import jax.numpy as jnp
+	def test_cyipopt_import(self):
+		import cyipopt
+
+
+	def _test_jax_import(self):
+		import jax.numpy as jnp
